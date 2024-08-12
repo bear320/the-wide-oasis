1090002495e6ad9c4e841d5833dcb6fc7bea48e4
bear320 <aaabear320@gmail.com>
2024-08-12 15:43:26 +0800

    feat: 🎸 Add global styles and customize heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import styled from "styled-components";
-
-const H1 = styled.h1`
-  font-size: 2rem;
-  font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 0.5rem;
-  background-color: dimgray;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 0.25rem;
-  padding: 0.8rem 1.2rem;
-`;
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const StyledApp = styled.div`
   background-color: lightgray;
@@ -30,11 +11,16 @@ const StyledApp = styled.div`
 
 const App = () => {
   return (
-    <StyledApp>
-      <H1>The Wide Oasis</H1>
-      <Button onClick={() => alert("Check In")}>Check In</Button>
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wide Oasis</Heading>
+        <Heading as="h2">The Wide Oasis</Heading>
+        <Heading as="h3">The Wide Oasis</Heading>
+        <Button onClick={() => alert("Check In")}>Check In</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 };
 
