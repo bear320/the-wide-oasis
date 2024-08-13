910bdc0dcd4b0bae272dcd87b4657a3fa1850e35
bear320 <aaabear320@gmail.com>
2024-08-13 15:41:51 +0800

    feat: 🎸 Fetch and store data via useQuery hook

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface ICabin {
+  id: number;
+  name: string;
+  description: string;
+  maxCapacity: number;
+  regularPrice: number;
+  discount: number;
+  image: string;
+  created_at: string;
+}
