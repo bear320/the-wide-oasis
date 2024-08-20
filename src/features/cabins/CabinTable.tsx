8c841096d744de329867c02595f7e994c0cf1e7d
bear320 <aaabear320@gmail.com>
2024-08-20 10:51:35 +0800

    feat: 🎸 Build a reusable filter

--- src/features/cabins/CabinTable.tsx
+++ src/features/cabins/CabinTable.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import { useCabins } from "./useCabins";
 import { ICabin } from "../../types";
 import Spinner from "../../ui/Spinner";
@@ -6,9 +7,24 @@ import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 
 const CabinTable = () => {
+  const [searchParams] = useSearchParams();
   const { cabins, isFetching } = useCabins();
 
   if (isFetching) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins: ICabin[];
+
+  if (filterValue === "all") filteredCabins = [...(cabins as ICabin[])];
+  else if (filterValue === "with-discount")
+    filteredCabins = (cabins as ICabin[])?.filter(
+      (cabin) => cabin.discount > 0
+    );
+  else if (filterValue === "no-discount")
+    filteredCabins = (cabins as ICabin[])?.filter(
+      (cabin) => cabin.discount === 0
+    );
 
   return (
     <Menus>
@@ -22,7 +38,10 @@ const CabinTable = () => {
           <div></div>
         </Table.Header>
 
-        <Table.Body data={cabins!} render={(cabin: ICabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
+        <Table.Body
+          data={filteredCabins!}
+          render={(cabin: ICabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
       </Table>
     </Menus>
   );
